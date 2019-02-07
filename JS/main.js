289dd3579175c1f3abd2a238cf885d509ccd8c84
jmendez44 <jenuel.mendez@gmail.com>
2019-02-07 12:24:09 -0500

    added event listener for nav links

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,12 @@
+//carousel
 let i = 0;
 let images = [];
 let time = 3000;
-
-
-
 
 images[0] = '/IMAGES/Webp.net-resizeimage (1).jpg';
 images[1] = '/IMAGES/adventure-ball-shaped-blur-346885.jpg';
 images[2] = '/IMAGES/school-children.jpg';
 images[3] = '/IMAGES/book-bookcase-books-1166657.jpg';
-
-
-
-console.log(images);
 
 function changeImg(){
     document.slide.src = images[i];
@@ -22,15 +16,19 @@ function changeImg(){
     } else {
         i = 0;
     }
-
     setTimeout('changeImg()', time);
-}
+};
 
 window.onload = changeImg;
 
 
 
-document.querySelector('#banner .button').addEventListener('mouseover', function () {
-    document.querySelector('#banner .button').style.background = 'white';
-});
+// nav hover link color
+let element = document.querySelectorAll('nav a');
 
+let arr = Array.from(element);
+
+arr.forEach((link) => {link.addEventListener("mouseover", function(){
+    console.log();
+    this.style.color = "#6C5359"; setTimeout( () => {this.style.color ="";}, 500);}, false)
+});
